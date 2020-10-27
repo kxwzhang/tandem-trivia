941fea73b0fc6c393da943691a1228d1e2726e9f
kxwzhang <kxwzhang@gmail.com>
2020-10-27 16:49:07 -0700

    Design the layout with a outer container

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import data from './data/Apprentice_TandemFor400_Data.json';
 
 function App() {
   return (
-    <div>
+    <div class='trivia-container'>
       <h1>Tandem Trivia App</h1>
+      <div>
+        {/* Place questions here */}
+      </div>
     </div>
   );
 }
