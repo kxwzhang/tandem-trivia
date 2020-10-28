a6edb2a154f76155d6dea070277e4a058b8ca94a
kxwzhang <kxwzhang@gmail.com>
2020-10-27 18:15:27 -0700

    On page load, shuffle the incoming questions array, then grab the first 10 and set it in the local state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './reset.css';
 import './trivia.css';
-import data from './data/Apprentice_TandemFor400_Data.json';
 
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [questions, setQuestions] = useState([]);
+
+  function generateRandomQuestions(questions) {
+    /* 
+    Duplicate original questions array then sort based 
+    on the difference between a random number and 0.5
+    */
+    const shuffled = [...questions].sort(() => Math.random() - 0.5);
+
+    // Get sub-array of first 10 elements after shuffling
+    return shuffled.slice(0, 10);
+  }
+
+  useEffect(() => {
+    fetch('./Apprentice_TandemFor400_Data.json')
+      .then(res => res.json())
+      .then(data => setQuestions(generateRandomQuestions(data)));
+  }, []);
+
+  console.log(questions);
 
   return (
     <div className='trivia-container'>
