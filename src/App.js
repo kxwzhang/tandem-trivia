b523ac0b63bd925ebb0e9d585ce01e78408082bf
kxwzhang <kxwzhang@gmail.com>
2020-10-27 20:11:01 -0700

    Update the question after 3 seconds

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { Trivia } from './components/trivia';
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [score, setScore] = useState(0);
 
   function generateRandomQuestions(questions) {
-    /* 
-    Duplicate original questions array then sort based 
-    on the difference between a random number and 0.5
-    */
+    /* Duplicate original questions array then sort based 
+    on the difference between a random number and 0.5 */
     const shuffledQuestions = [...questions].sort(() => Math.random() - 0.5);
     // Get sub-array of first 10 elements after shuffling
     return shuffledQuestions.slice(0, 10);
-  }
+  };
 
   useEffect(() => {
     fetch('./Apprentice_TandemFor400_Data.json')
@@ -22,13 +22,27 @@ function App() {
       .then(data => setQuestions(generateRandomQuestions(data)));
   }, []);
 
-  console.log(questions);
+  function handleUpdate(answer) {
+    console.log('clicking');
+    // If the answer is correct update our score
+    if (answer === questions[questionNumber].correct) setScore(score + 1000);
+    if (questionNumber < 10) {
+      // After 3 seconds, update to next question
+      setTimeout(() => setQuestionNumber(questionNumber + 1), 3000);
+    } else {
+      // Reset the game after the last question has been reached 
+    }
+  };
 
   return questions.length ? (
     <div className='trivia-container'>
-      <Trivia triviaQuestion={questions[0]} />
+      <h1 className='trivia-title'>Tandem Trivia-Hoot!</h1>
+      <h2 className='score'>Score: {score}</h2>
+      <Trivia 
+        triviaQuestion={questions[questionNumber]}
+        handleUpdate={handleUpdate} />
     </div>
   ) : (<div></div>);
-}
+};
 
 export default App;
--- src/components/trivia.js
+++ src/components/trivia.js
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 
-export const Trivia = ({ triviaQuestion: { question, correct, incorrect }}) => {
-  const [currQuestion, setCurrQuestion] = useState(undefined);
-
+export const Trivia = ({ handleUpdate, triviaQuestion: { question, correct, incorrect }}) => {
   const shuffledAnswers = [correct, ...incorrect].sort(() => Math.random() - 0.5);
-
-  function handleAnswer(answer) {
-    console.log();
-    setCurrQuestion(question);
-  }
 
   return (
     <div>
@@ -19,26 +12,10 @@ export const Trivia = ({ triviaQuestion: { question, correct, incorrect }}) => {
         {shuffledAnswers.map((answer, idx) => {
           return (<button
             key={idx} 
-            onClick={() => handleAnswer(answer)}
+            onClick={() => handleUpdate(answer)}
             className='trivia-answer-button'>{answer}</button>
           );
         })}
-        {/* <button
-          className='temp'
-          onClick={() => handleAnswer(shuffledAnswers[0])} 
-          className='trivia-answer-button'>1) {shuffledAnswers[0]}</button>
-        <button
-          className='temp'
-          onClick={() => handleAnswer(shuffledAnswers[1])} 
-          className='trivia-answer-button'>2) {shuffledAnswers[1]}</button>
-        <button
-          className='temp'
-          onClick={() => handleAnswer(shuffledAnswers[2])} 
-          className='trivia-answer-button'>3) {shuffledAnswers[2]}</button>
-        <button
-          className='temp'
-          onClick={() => handleAnswer(shuffledAnswers[3])} 
-          className='trivia-answer-button'>4) {shuffledAnswers[3]}</button> */}
       </div>
     </div>
   );
