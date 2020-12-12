0de5b28919a8227ff120780de9a1ac3d247cbe58
kxwzhang <kxwzhang@gmail.com>
2020-12-12 15:22:48 -0800

    Add SideNav styled component

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const SideNav = styled.div`
+  height: 100%;
+  width: 0; 
+  position: fixed; 
+  z-index: 1; 
+  top: 0; 
+  left: 0;
+  background-color: #111; 
+  overflow-x: hidden; 
+  padding-top: 60px; 
+  transition: 0.5s;
+`;
+
+// const 
+
 export const Sidebar = () => {
   return (
-    <div>
+    <SideNav>
 
-    </div>
+    </SideNav>
   );
 };
