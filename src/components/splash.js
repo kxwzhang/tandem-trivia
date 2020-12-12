412c5297293b6511df2b3debe5b56b46b0eba1a8
kxwzhang <kxwzhang@gmail.com>
2020-12-12 14:21:01 -0800

    Style the start button with styled-components

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,4 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const RenderButton = styled.button`
+  font-size: 16px;
+  border-radius: 5px;
+  border: none;
+  padding: 8px;
+  transition: background-color 0.25s, color 0.25s;
+  margin-top: 8px;
+
+  &:hover {
+    background-color: rgb(223, 170, 223);
+  }
+`;
 
 export const Splash = ({ setRenderSplash }) => {
   return (
@@ -6,7 +20,7 @@ export const Splash = ({ setRenderSplash }) => {
       <div className='splash-items'>
         Welcome to Tandem Trivia! Are you ready to play a game?
       </div>
-      <button onClick={() => setRenderSplash(false)}>Let's get started</button>
+      <RenderButton onClick={() => setRenderSplash(false)}>Let's get started</RenderButton>
     </div>
   )
 };
