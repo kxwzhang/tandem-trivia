c68941643364277a895795f8df92a198e25679f5
kxwzhang <kxwzhang@gmail.com>
2020-12-12 13:08:01 -0800

    Design placeholder text for splash

--- src/components/splash.js
+++ src/components/splash.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export const Splash = () => {
   return (
-    <div>
-      
+    <div className='splash-container'>
+      <div className='splash-items'>
+        Welcome to Tandem Trivia! Are you ready to play a game?
+      </div>
+      <button>Let's get started</button>
     </div>
   )
 };
